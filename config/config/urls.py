12d8580d308285dddda42465d8f8506ae921a3dd
Yeram <ramye@naver.com>
2021-08-04 21:46:37 +0900

    [CREATE] review_form/review_list_page

--- config/config/urls.py
+++ config/config/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cafe/', include('cafe.urls')),
+] 
     #path('', include('user.urls')),
     path('accounts/', include('accounts.urls')),
 ]
