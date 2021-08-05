b42c65e2faab913d315fbd8e3f6394390b8c01cc
Yeram <ramye@naver.com>
2021-08-05 11:14:31 +0900

    [FIX] review_form,list

--- config/cafe/admin.py
+++ config/cafe/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CafeList, Review, Comment, Map
 
-# Register your models here.
+# Register your models here
+admin.site.register(CafeList)
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Map)
--- config/cafe/views.py
+++ config/cafe/views.py
@@ -4,26 +4,26 @@ from .forms import ReviewForm
 
 # Create your views here.
 def review_list(request, pk):
-    cafe = CafeList.objects.all(pk=pk) #카페 정보 불러옴
-    reviews = Review.objects.filter(cafe_review=cafe.name) #해당 카페 리뷰만 불러옴
-    review_comments = Comment.objects.all(review_comment=reviews.id)
+    cafe = CafeList.objects.all(pk=pk) #해당 카페 정보 불러옴
+    reviews = Review.objects.filter(cafe_review=cafe.id) #해당 카페 리뷰만 불러옴
+    review_comments = Comment.objects.filter(review_comment=reviews.id)
     
     cafe_stars = []
-    for i  in range(cafe.stars):
+    for i  in range(cafe.cafe_stars): #소숫점 때문에 별이 잘 안나올듯...
         cafe_stars.append('⭐')
         i += 1
 
     ctx={'cafe': cafe, 'reviews': reviews, 'cafe_stars':cafe_stars, 'review_comments': review_comments}
-    return render(request, template_name='review_list.html', context=ctx)
+    return render(request, 'cafe/review_list.html', ctx)
 
 
 def review_create(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = form.save()
+            form.save()
             return redirect('cafe:review_list')
     else:
         form = ReviewForm()
         ctx = {'form': form}
-        return render(request, template_name='review_form.html', context=ctx)
+        return render(request, 'cafe/review_form.html', ctx)
