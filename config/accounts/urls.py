9206a0cae4ec548aefdea8799f435c24529bd546
Jeonghoon Park <shb03323@naver.com>
2021-08-08 16:05:35 +0900

    [FIX] config.urls

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-#app_name = "accounts"
+app_name = "accounts"
 
 urlpatterns = [
     path('', views.main, name='main'),
--- config/config/urls.py
+++ config/config/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cafe.urls')),
+    path('cafe/', include('cafe.urls')),
 
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('account/', include('allauth.urls')), #accounts.urls
     path('enroll/', include('enroll.urls')),
 ]
