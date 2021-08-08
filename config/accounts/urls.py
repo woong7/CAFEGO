edd74ac51c96170d6120a6601d1649b98f3b1e6d
Yeram <ramye@naver.com>
2021-08-08 23:57:58 +0900

    [CREATE]searchfunction

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('rank/list/', views.rank_list, name='rank_list'),
 
     path('enroll_home/', views.enroll_home, name="enroll_home"),
-    path('enroll_new_cafe/', views.enroll_new_cafe, name="enroll_new_cafe"),
-    path('enroll_visited_cafe/', views.enroll_visited_cafe, name="enroll_visited_cafe"),
+    path('enroll_new_cafe/', views.EnrollNewCafeListView.as_view(), name="enroll_new_cafe"),
+    path('enroll_visited_cafe/', views.EnrollVisitedCafeListView.as_view(), name="enroll_visited_cafe"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- config/accounts/views.py
+++ config/accounts/views.py
@@ -1,15 +1,19 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
 #from django.contrib.auth.models import User #회원가입을 구현하는데 있어 장고가 제공해주는 편리함
 from django.urls import reverse
 from django.contrib import auth
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from .models import * #User
+from cafe.models import CafeList
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as django_logout
 from . import forms
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 #없어도 될듯,,,
@@ -90,27 +94,128 @@ def rank_list(request):
 def enroll_home(request):
     return render(request, "accounts/enroll_home.html")
 
-def enroll_new_cafe(request):
-    cafe_list = VisitedCafe.objects.all() #user id 넣어서 그 값만 가져와야 함
-    if request.method == 'POST':
-        form = forms.VisitedCafeForm(request.POST, request.FILES)
+class EnrollNewCafeListView(ListView):
+    model = CafeList
+    paginate_by = 5
+    template_name = 'accounts/enroll_new_cafe.html'
+    context_object_name = 'new_cafe_list'
 
-        if form.is_valid():
-            ##저장
-            cafe = VisitedCafe()
-            cafe.user = form.cleaned_data['user']
-            cafe.cafename = form.cleaned_data['cafename']
-            cafe.visit_count = form.cleaned_data['visit_count']
-            cafe.cafe_id = form.cleaned_data['cafe_id']
-            cafe.save()
-    else:
-        form = forms.VisitedCafeForm()
-    
-    return render(request, 'accounts/enroll_new_cafe.html', {
-        'cafe_list': cafe_list,
-        'form': form,
-    })
+    #등록기능?
+    def enroll_new_cafe(request):
+        cafe_list = VisitedCafe.objects.all() #user id 넣어서 그 값만 가져와야 함
+        if request.method == 'POST':
+            form = forms.VisitedCafeForm(request.POST, request.FILES)
 
-def enroll_visited_cafe(request):
-    return render(request, "accounts/enroll_visited_cafe.html")
+            if form.is_valid():
+                ##저장
+                cafe = VisitedCafe()
+                cafe.user = form.cleaned_data['user']
+                cafe.cafename = form.cleaned_data['cafename']
+                cafe.visit_count = form.cleaned_data['visit_count']
+                cafe.cafe_id = form.cleaned_data['cafe_id']
+                cafe.save()
+        else:
+            form = forms.VisitedCafeForm()
+        
+        return render(request, 'accounts/enroll_new_cafe.html', {
+            'cafe_list': cafe_list,
+            'form': form,
+        })
 
+    #검색 기능
+    def get_queryset(self):
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '') 
+        new_cafe_list = CafeList.objects.order_by('-id')#나중에 ㄱㄴㄷ 순으로 바꿀?
+
+        if search_keyword:
+            if len(search_keyword) > 1:
+                if search_type == 'name':
+                    search_cafe_list = new_cafe_list.filter(name__icontains=search_keyword)
+                elif search_type == 'address':
+                    search_cafe_list = new_cafe_list.filter(address__icontains=search_keyword)
+                elif search_type == 'all':
+                    search_cafe_list = new_cafe_list.filter(Q(name__icontains=search_keyword) | Q(address__icontains=search_keyword))
+                return search_cafe_list
+        else:
+            messages.error(self.request, '2글자 이상 입력해주세요.')
+        return new_cafe_list
+
+    #하단부에 페이징 처리
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '') 
+
+        if len(search_keyword) > 1:
+            context['q'] = search_keyword
+        context['type'] = search_type
+
+        return context
+
+class EnrollVisitedCafeListView(ListView):
+    model = VisitedCafe
+    paginate_by = 5
+    template_name = 'accounts/enroll_visited_cafe.html'
+    context_object_name = 'visited_cafe_list'
+
+    #검색 기능
+    def get_queryset(self):
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '') 
+        visited_cafe_list = CafeList.objects.order_by('-id')#나중에 ㄱㄴㄷ 순으로 바꿀?
+
+        if search_keyword:
+            if len(search_keyword) > 1:
+                if search_type == 'name':
+                    search_cafe_list = visited_cafe_list.filter(name__icontains=search_keyword)
+                elif search_type == 'address':
+                    search_cafe_list = visited_cafe_list.filter(address__icontains=search_keyword)
+                elif search_type == 'all':
+                    search_cafe_list = visited_cafe_list.filter(Q(name__icontains=search_keyword) | Q(address__icontains=search_keyword))
+                return search_cafe_list
+        else:
+            messages.error(self.request, '2글자 이상 입력해주세요.')
+        return visited_cafe_list
+
+    #하단부에 페이징 처리
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '') 
+
+        if len(search_keyword) > 1:
+            context['q'] = search_keyword
+        context['type'] = search_type
+
+        return context
