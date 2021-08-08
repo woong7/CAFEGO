b4f5f75eb756f93c5fa7d3e7190a6f7feabc5c9b
sehyeon_kim <shgim9977@gmail.com>
2021-08-08 16:07:07 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -21,6 +21,4 @@ urlpatterns = [
     path('enroll_home/', views.enroll_home, name="enroll_home"),
     path('enroll_new_cafe/', views.enroll_new_cafe, name="enroll_new_cafe"),
     path('enroll_visited_cafe/', views.enroll_visited_cafe, name="enroll_visited_cafe"),
-
-
 ]
