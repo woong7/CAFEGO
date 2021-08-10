07a6cb9508e58b4b22a417c229337515a13c52cf
sehyeon_kim <shgim9977@gmail.com>
2021-08-10 10:49:48 +0900

    [Fix] 약간의 충돌 수정

--- config/accounts/models.py
+++ config/accounts/models.py
@@ -87,11 +87,6 @@ class Badge(models.Model):
     badge_name=models.TextField(max_length=150, unique=True)
     badge_image=models.ImageField()
     
-    
-    cafename = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.cafename
 
 #방문한 카페에서 먹은 음료 정보 -> 나중에 objects.all로 가져오기
 class Drink(models.Model):
@@ -100,6 +95,3 @@ class Drink(models.Model):
 
     def __str__(self):
         return self.drinkname
-
-    
-    
