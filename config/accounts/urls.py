741888e847badf514887ba32163bb205842d8ba7
woong7 <rjsdnd980316@naver.com>
2021-08-11 23:07:54 +0900

    Friend : follow / unfollow / search

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('mypage/<int:pk>', views.mypage, name='mypage'),
     path('addfriend/<int:pk>', views.addfriend, name='addfriend'),
+    path('deletefriend/<int:pk>', views.deletefriend, name='deletefriend'),
+    path('friend_search', views.FriendSearchListView.as_view(), name='friend_search'),
+    path('friend_register/', views.friend_register, name="friend_register"),
 
     path('badge/list/', views.badge_list, name='badge_list'),
     path('badge/taken/', views.badge_taken, name='badge_taken'),
@@ -25,6 +28,7 @@ urlpatterns = [
 
     path('enroll_home/', views.enroll_home, name="enroll_home"),
     path('visit_register/', views.visit_register, name="visit_register"),
+    path('visited_register/', views.visited_register, name="visited_register"),
     path('enroll_new_cafe/', views.EnrollNewCafeListView.as_view(), name="enroll_new_cafe"),
     path('enroll_visited_cafe/', views.EnrollVisitedCafeListView.as_view(), name="enroll_visited_cafe"),
     
