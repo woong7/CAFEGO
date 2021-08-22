29dd29a611dd0e6b1fb977091e6f7417f26588b1
woong7 <rjsdnd980316@naver.com>
2021-08-22 18:29:26 +0900

    수정

--- config/config/settings.py
+++ config/config/settings.py
@@ -173,8 +173,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pirocafego@gmail.com' #'kgw980316@gmail.com'
-EMAIL_HOST_PASSWORD = 'pirogramming15!' #'parabe1!um'
+EMAIL_HOST_USER = 'pirocafego@gmail.com' 
+EMAIL_HOST_PASSWORD = 'pirogramming15!' 
 EMAIL_USE_TLS = True
 
 CORS_ORIGIN_ALLOW_ALL = True
